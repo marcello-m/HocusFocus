92bf08375da1ee32e1fa0003f141dca21267b80b
marcello-m <marcello-m@users.noreply.github.com>
2023-05-28 14:19:18 +0200

    scripts: now the user can insert the domain with or without the "www." and obtain the same result

--- scripts/content.js
+++ scripts/content.js
@@ -5,8 +5,13 @@ chrome.storage.sync.get(['blockedDomains'], function(result) {
     const blockedDomains = result.blockedDomains;
   
     // Check if the current tab's domain is in the blocked domains list
-    if (blockedDomains && blockedDomains.includes(location.hostname)) {
+    if (blockedDomains && (blockedDomains.includes(normalizeDomain(location.hostname)) || blockedDomains.includes(location.hostname))) {
       // Perform actions to block distractions
       document.body.innerHTML = '<h1>Focus!</h1>';
     }
 });
+
+// Function to normalize the domain by removing leading "www."
+function normalizeDomain(domain) {
+  return domain.replace(/^www\./i, '');
+}
