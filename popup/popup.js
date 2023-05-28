b696081bbe32865b62cdb932bac6b1b913a5516b
marcello-m <marcello98manenti@gmail.com>
2023-05-28 15:57:03 +0200

    popup: added ON/OFF button to turn the extension on and off

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
 var dottedListElement = document.getElementById("dottedList"); // Dotted list element
+const toggleButton = document.getElementById("toggleButton"); // Toggle switch element 
 
 dottedListElement.innerHTML = "";
 
@@ -12,4 +13,27 @@ chrome.storage.sync.get(['blockedDomains'], function (result) {
             dottedListElement.appendChild(listItem);
         });
     }
+});
+
+chrome.storage.sync.get(['isEnabled'], function (result) {
+    const isEnabled = result.isEnabled;
+
+    // Set the initial text and appearance of the button based on the isEnabled value
+    toggleButton.textContent = isEnabled ? 'ON' : 'OFF';
+    toggleButton.classList.toggle('active', isEnabled);
+});
+
+toggleButton.addEventListener('click', function () {
+
+    chrome.storage.sync.get(['isEnabled'], function (result) {
+        const isEnabled = result.isEnabled;
+
+        const newState = !isEnabled;
+
+        // Update the text and appearance of the button based on the new state
+        toggleButton.textContent = newState ? 'ON' : 'OFF';
+        toggleButton.classList.toggle('active', newState);
+
+        chrome.storage.sync.set({ isEnabled: newState });
+    });
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,17 @@
 let blockedDomains = [];
 
 // Retrieve blocked domains from storage
-chrome.storage.sync.get(['blockedDomains'], function(result) {
-    const blockedDomains = result.blockedDomains;
-  
+chrome.storage.sync.get(['blockedDomains', 'isEnabled'], function (result) {
+  const blockedDomains = result.blockedDomains;
+  const isEnabled = result.isEnabled;
+
+  if (isEnabled) {
     // Check if the current tab's domain is in the blocked domains list
     if (blockedDomains && (blockedDomains.includes(normalizeDomain(location.hostname)) || blockedDomains.includes(location.hostname))) {
       // Perform actions to block distractions
       document.body.innerHTML = '<h1>Focus!</h1>';
     }
+  }
 });
 
 // Function to normalize the domain by removing leading "www."
